b020391c9115da419f13818aa2ca9737d39214f7
vardaans60-star <vardaans60@gmail.com>
2025-09-16 13:57:55 +0530

    Q6.c

---
+++ Q6.c
@@ -0,0 +1,15 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Choose integer:");
+	scanf("%d",&a);
+	printf("The value of your chosen integer is:%d",a);
+
+}
