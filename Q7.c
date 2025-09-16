50a3db7d4b313b0cc4f5fafc14b552a741e645a6
vardaans60-star <vardaans60@gmail.com>
2025-09-16 13:59:10 +0530

    Q7.c

---
+++ Q7.c
@@ -0,0 +1,14 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+  float a;
+  printf("Enter your no. :");
+  scanf("%f",&a);
+  printf("Your chosen no. is :%.2f",a);
+}
