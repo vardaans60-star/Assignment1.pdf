c6a36d33be35a7fb7dbed5968a8f61366f1f5178
vardaans60-star <vardaans60@gmail.com>
2025-09-16 14:06:36 +0530

    Q10.c

---
+++ Q10.c
@@ -0,0 +1,24 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include <stdio.h>
+int main(){
+    int a,b,c,d,e,f,g;
+    printf("Choose your value of a:");
+    scanf("%d",&a);
+    printf("Choose your value of b:");
+    scanf("%d",&b);
+    c=a+b;
+    d=a-b;
+    e=a%b;
+    f=a*b;
+    g=a/b;
+    printf("The addition is:%d\n",c);
+    printf("The subtraction is :%d\n",d);
+    printf("The remainder is :%d\n",e);
+    printf("The multiplication is:%d\n",f);
+    printf("The division is:%d",g);
+}
