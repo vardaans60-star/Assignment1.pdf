0acb1b2e73f8c9f688189ec5cc8f977a45ab3d48
vardaans60-star <vardaans60@gmail.com>
2025-09-16 14:05:05 +0530

    Q9.c

---
+++ Q9.c
@@ -0,0 +1,15 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include <stdio.h>
+int main(){
+    int numerator,denominator;
+    printf("Enter your numerator:\n");
+    scanf("%d",&numerator);
+    printf("Enter your denominator:\n");
+    scanf("%d",&denominator);
+    printf("the fraction is:%d/%d",numerator,denominator);
+}
