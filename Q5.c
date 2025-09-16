1ca7935589b58d00a267de43ed91eb74bbccbae9
vardaans60-star <vardaans60@gmail.com>
2025-09-16 13:56:21 +0530

    Q5.c

---
+++ Q5.c
@@ -0,0 +1,13 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	printf("The other names of console display are:\n1.Terminal\n2.Command Prompt\n3.Command Line\n4.Shell\n5.Command Line Interference");
+	
+}
