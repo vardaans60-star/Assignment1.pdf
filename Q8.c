0f6be9e63dcbbc29042ac75848f9067c67933066
vardaans60-star <vardaans60@gmail.com>
2025-09-16 14:00:35 +0530

    Q8.c

---
+++ Q8.c
@@ -0,0 +1,14 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+  char a='t';
+  printf("Choose your character:");
+  scanf("%c",&a);
+  printf("The chosen character is:%c",a);
+}
